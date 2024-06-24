aee0eef1a9f984c6b5e0077397db7f81e37e8ec4
Ruhul Amin <ruhul.computer12@gmail.com>
2024-06-25 00:13:24 +0600

    Another Solution
     - How Many Numbers Are Smaller than the current number.

--- Leetcode/1365. How Many Numbers Are Smaller Than the Current Number/1365. How Many Numbers Are Smaller Than the Current Number.cpp
+++ Leetcode/1365. How Many Numbers Are Smaller Than the Current Number/1365. How Many Numbers Are Smaller Than the Current Number.cpp
@@ -4,6 +4,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int n = nums.size(), i;
+        vector<int> ans = nums;
+        sort(nums.begin(), nums.end());
+        unordered_map<int, int> map;
+        for(i = n - 1; i >= 0; i--){
+            map[nums[i]] = i;
+        }
+
+        for(i = 0; i < n; i++){
+            ans[i] = map[ans[i]];
+        }
+
+/*
         int n = nums.size(), answer = 0;
         vector<int> ans(n);
         for (int i = 0; i < n; i++) {
@@ -14,7 +27,7 @@ public:
                 }
             }
             ans[i] = answer;
-        }
+        }*/
         for(int i = 0; i < n; i++){
             cout << ans[i] << " ";
         }
