8401ffe0b359e87a2e3fd8bcdfefa86fe54f39b5
Ruhul Amin <ruhul.computer12@gmail.com>
2024-06-24 23:35:29 +0600

    (Two pointer approach) Count Pairs whose sum is less than target

--- Leetcode/2824. Count Pairs Whose Sum is Less than Target/2824. Count Pairs Whose Sum is Less than Target.cpp
+++ Leetcode/2824. Count Pairs Whose Sum is Less than Target/2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -3,14 +3,31 @@ using namespace std;
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
-        int i, j, ans = 0, n = nums.size();
+        int l = 0, r = nums.size()-1;
+        int ans = 0;
+
+        sort(nums.begin(), nums.end());
+        while(l < r){
+            if((nums[l] + nums[r] ) < target)
+            {
+                ans += (r - l);
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+
+        return ans;
+        /*int i, j, ans = 0, n = nums.size();
         for(i = 0; i < n; i++){
             for(j = i + 1; j < n; j++){
                 if(nums[i]+nums[j] < target)
                     ans++;
             }
         }
-        return ans;
+        return ans;*/
     }
 };
 
