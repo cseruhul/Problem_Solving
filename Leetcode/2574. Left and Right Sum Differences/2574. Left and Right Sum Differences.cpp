d2ec939bc17593f66c442d2592f1fd1383e8af59
Ruhul Amin <ruhul.computer12@gmail.com>
2024-06-25 17:30:14 +0600

    Left and Right Sum.

--- Leetcode/2574. Left and Right Sum Differences/2574. Left and Right Sum Differences.cpp
+++ Leetcode/2574. Left and Right Sum Differences/2574. Left and Right Sum Differences.cpp
@@ -3,6 +3,30 @@ using namespace std;
 class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
+    int n, i;
+    n = nums.size();
+    vector<int>leftSum(n);
+    vector<int>rightSum(n);
+    vector<int>answer(n);
+
+    leftSum[0] = rightSum[n - 1] = 0;
+
+    for(i = 1; i < n; i++)
+        leftSum[i] = leftSum[i - 1] + nums[i - 1];
+
+    for(i = n - 2; i >= 0; i--)
+        rightSum[i] = rightSum[i + 1] + nums[i + 1];
+
+    for(i = 0; i < n; i++)
+        answer[i] = abs(leftSum[i] - rightSum[i]);
+
+    for(i = 0; i < n; i++)
+            cout << answer[i] << " ";
+
+
+    /*
+
+
         int i, n, left, right, j;
         n = nums.size();
         vector<int> answer(n);
@@ -21,6 +45,7 @@ public:
         return answer;
         for(i = 0; i < n; i++)
             cout << answer[i] << " ";
+    */
     }
 };
 
