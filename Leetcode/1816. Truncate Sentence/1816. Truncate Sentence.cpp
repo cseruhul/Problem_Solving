d8b21c8341410cbf415f7a09c19839d718ea70a8
Ruhul Amin <ruhul.computer12@gmail.com>
2024-06-30 01:43:03 +0600

    Solved. Truncate Sentence

--- Leetcode/1816. Truncate Sentence/1816. Truncate Sentence.cpp
+++ Leetcode/1816. Truncate Sentence/1816. Truncate Sentence.cpp
@@ -6,24 +6,30 @@ public:
     string truncateSentence(string s, int k) {
         int space = 0, i = 0;
         string answer;
-        for(auto item : s){
-            if(item == ' '){
+        for (i = 0; i < s.size(); i++) {
+            answer += s[i];
+            cout << s[i];
+            if (s[i+1] == ' ') {
                 space++;
             }
-            if(item == k){
-
+            if (space == k) {
+                break;
             }
-
         }
 
-        cout << space;
+
+        for(auto item : answer){
+            cout << item ;
+        }
+        cout << ";check;";
+        return answer;
 
     }
 };
 
 int main(){
-    string s = "Hello how are you Contestant";
-    int k = 4;
+    string s = "chopper is not a tanuki";
+    int k = 5;
     Solution solN;
 
     solN.truncateSentence(s, k);
