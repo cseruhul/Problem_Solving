bc214063de5a8470f356c2d5f231460108804f18
Ruhul Amin <ruhul.computer12@gmail.com>
2024-07-08 15:32:18 +0600

    Last Stone WEight
     Another solution usgin priority queue.

--- Leetcode/1046. Last Stone Weight/1046. Last Stone Weight.cpp
+++ Leetcode/1046. Last Stone Weight/1046. Last Stone Weight.cpp
@@ -3,34 +3,31 @@ using namespace std;
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        int x, y;
-        if(stones.size() == 1){
-            return stones[0];
-        }else{
-            sort(stones.begin(), stones.end(), greater<int>());
-            y = stones[0];
-            x = stones[1];
-
-            while(x != 0){
-                stones[0] = y - x;
-                stones[1] = 0;
-
-                sort(stones.begin(), stones.end(), greater<int>());
-
-                y = stones[0];
-                x = stones[1];
-
-            }
-            return stones[0];
+        priority_queue<int> stone;
+        for(auto item:stones){
+            stone.push(item);
         }
-
+        while(stone.size() != 0){
+            if(stone.size() == 1){
+                return stone.top();
+            }
+            else{
+                int y = stone.top();
+                stone.pop();
+                int x =stone.top();
+                stone.pop();
+                if(x != y)
+                    stone.push(y - x);
+            }
+        }
+        return 0;
     }
 };
 
 int main(){
     Solution soln;
-    vector<int> stones{2};
+    vector<int> stones{2,7,4,1,8,1};
 
-    cout << soln.lastStoneWeight(stones);
+    cout  << soln.lastStoneWeight(stones);
     return 0;
 }
